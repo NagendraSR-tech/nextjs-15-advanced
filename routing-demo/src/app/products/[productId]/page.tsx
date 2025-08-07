d67a85e9945ff0f42e224ee91106ad34b2a403f2
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 03:04:56 +0530

    Routing metadata - Handling metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,9 +1,25 @@
-export default async function ProductDetails({
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
-  const productId = await params.productId;
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product - ${id}`);
+    }, 100); // Simulating a delay for fetching metadata
+  });
+  return {
+    title: `Product - ${title}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
+  const productId = (await params).productId;
   // Simulate fetching product details based on productId
   return <h1>Details about Product {productId}</h1>;
 }
