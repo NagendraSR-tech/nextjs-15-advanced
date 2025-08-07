1f1a6cf60532f09236831bd3cf755cd85a97d157
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 02:28:08 +0530

    added multiple route layout example

---
+++ multiple-route-layouts/src/app/(marketing)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Multiple Route Layouts</h1>;
+}
---
+++ multiple-route-layouts/src/app/footer/page.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <div style={{ backgroundColor: "ghostwhite", padding: "1rem" }}>Footer</div>
+  );
+}
