56ff643192afe9385e9d73ffe9aa44af05899ee2
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 03:14:57 +0530

    Title Metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog", //overside the default title
+  },
+};
+
 export default function Blog() {
   return <h1>Blog</h1>;
 }
