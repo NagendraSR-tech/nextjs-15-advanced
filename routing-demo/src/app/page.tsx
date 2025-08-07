63c19b244251d48a340bb6ccf47c6bbf6a410982
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 03:40:42 +0530

    Link Component

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome Next.js Home</h1>;
+  return (
+    <>
+      <h1>Welcome Next.js Home</h1>
+      <Link href="/products">Products</Link>
+      <br />
+      <Link href="/blog">Blog</Link>
+      <br />
+      <Link href="/blog/first-post">First Post</Link>
+      <br />
+    </>
+  );
 }
