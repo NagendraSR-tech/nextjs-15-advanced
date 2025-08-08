3adf41a52169f2812a18bd08bfd7ae57ebbb1386
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 14:00:05 +0530

    Templates - layuts and templates diff and usecase

--- routing-demo/src/app/(auth)/template.tsx
+++ routing-demo/src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -13,9 +14,17 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [input, setInput] = useState("");
   const pathname = usePathname();
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       {navLinks.map((link) => {
         const isActive =
           pathname === link.href ||
