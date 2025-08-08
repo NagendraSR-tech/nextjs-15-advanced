b1fcebb4ebc2ca1e395caf3bfd0f1174d6cf52a3
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-08 14:18:33 +0530

    loading page

---
+++ routing-demo/src/app/blog/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  return <h1>Loading...</h1>;
+}
+//can use it for loading skeleton loaders, spinners, preview etc.
--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -6,6 +6,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("delayed blog page");
+    }, 2000);
+  });
   return <h1>Blog</h1>;
 }
