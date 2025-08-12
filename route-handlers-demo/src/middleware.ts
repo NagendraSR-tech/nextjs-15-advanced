4b64936db4a371cbd1b5a709ff015bd77ee6c9d9
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-12 07:14:31 +0530

    middleware - use of cookies and headers in middleware

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -1,21 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   return NextResponse.redirect(new URL("/", request.url));
-// }
-
-// //approach 1 - matcher config approach
-// export const config = {
-//   matcher: "/profile",
-// };
-// //redirects to home page
-
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/hello", request.nextUrl));
-    //also supports url rewrites feature - (handling legacy url /imporving SEO )
-    //return NextResponse.rewrite(new URL("/hello", request.nextUrl));
-    // //keep the url unchanged while serving differnt content
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
+
+  response.headers.set("custom-header", "custom-value");
+
+  return response;
+  //     return NextResponse.redirect(new URL("/hello", request.nextUrl));
+  //     //also supports url rewrites feature - (handling legacy url /imporving SEO )
+  //     //return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+  //     // //keep the url unchanged while serving differnt content
 }
