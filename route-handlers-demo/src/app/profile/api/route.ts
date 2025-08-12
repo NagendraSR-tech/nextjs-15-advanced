b45d3eaba47cd438d0595decf7252e6d0b56aad3
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-12 05:51:55 +0530

    Cookies in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,17 +1,26 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers"; //headers from nextjs
+import { headers, cookies } from "next/headers"; //headers from nextjs
 
 //setting incoming request
 export async function GET(request: NextRequest) {
-  // const requestHeaders = new Headers(request.headers);
-  // console.log(requestHeaders.get("Authorization"));
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
   // return new Response("Profile API Data");
 
   const headerList = await headers();
   console.log(headerList.get("Authorization")); //To set headers you need to new response with custom headers
+
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"))
+
   return new Response("Profile API Data", {
     headers: {
       "Content-Type": "text/html",
+      "set-Cookie": "theme=dark",
     },
   });
 }
