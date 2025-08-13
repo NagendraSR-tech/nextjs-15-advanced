e74c8826b8a9fbdbe820a1249e7c6e6edef9fefc
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-13 16:11:53 +0530

    Client-only Code - Implement Client-Only fn, safeguard client only code

--- rendering-demo/src/app/client-route1/page.tsx
+++ rendering-demo/src/app/client-route1/page.tsx
@@ -1,36 +1,18 @@
 "use client";
 
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 import React from "react";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 
 export default function ClientRoute1Page() {
   const theme = useTheme();
-  //   const settings = {
-  //     dots: true,
-  //   };
+  const result = clientSideFunction();
+
   return (
     <>
       <h1 style={{ color: theme.colors.primary }}>Client Route1 page</h1>
-      {/* <div className="image-slider-container">
-        <Slider {...settings}>
-          <div>
-            <img src="http://picsum.photos/g/400/200" />
-          </div>
-          <div>
-            <img src="http://picsum.photos/g/400/200" />
-          </div>
-          <div>
-            <img src="http://picsum.photos/g/400/200" />
-          </div>
-          <div>
-            <img src="http://picsum.photos/g/400/200" />
-          </div>
-        </Slider>
-      </div> */}
+      <p>{result}</p>
     </>
   );
 }
--- rendering-demo/src/app/server-route1/page.tsx
+++ rendering-demo/src/app/server-route1/page.tsx
@@ -1,12 +1,15 @@
 import { serverSideFunction } from "@/utils/server-utils";
-
 import { ImageSlider } from "@/components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ServerRoute1Page() {
   const result = serverSideFunction();
+  const clientResult = clientSideFunction();
   return (
     <>
-      <h1>Server Route {result}</h1>;
+      <h1>
+        Server Route {result} {clientResult}
+      </h1>
       <ImageSlider />
     </>
   );
