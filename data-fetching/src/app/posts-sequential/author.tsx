8a5887792847842b584d3f891d1c0adc68c4ba04
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-14 13:44:08 +0530

    Sequential Data Fetching - fetch author using userId property

---
+++ data-fetching/src/app/posts-sequential/author.tsx
@@ -0,0 +1,20 @@
+type Author = {
+  id: number;
+  name: string;
+};
+
+export default async function AuthorPage({ userId }: { userId: number }) {
+  //   await new Promise((resolve) => setTimeout(resolve, 1000)); //test loading author name
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  const user: Author = await response.json();
+  return (
+    <div className="textsm text-gray-500">
+      written by:{" "}
+      <span className="  font-semibold text-gray-700 hover:text-gray-900 transition-colors">
+        {user.name}
+      </span>
+    </div>
+  );
+}
