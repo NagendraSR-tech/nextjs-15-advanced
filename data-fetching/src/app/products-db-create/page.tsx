6a1b2bf799024c13f296f2e9a20acc9e27cea906
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-14 21:48:10 +0530

    Form validation with useActionState - separate Server Actions

--- data-fetching/src/app/products-db-create/page.tsx
+++ data-fetching/src/app/products-db-create/page.tsx
@@ -1,43 +1,110 @@
-import { Submit } from "@/components/submit";
-import { addProduct } from "@/prisma-db";
-import { redirect } from "next/navigation";
-export default function AddProductPage() {
-  async function createProduct(formData: FormData) {
-    "use server";
-    const title = formData.get("title") as string;
-    const price = formData.get("price") as string;
-    const description = formData.get("description") as string;
+"use client";
 
-    await addProduct(title, parseInt(price), description);
-    redirect("/products-db");
-  }
+// import { Submit } from "@/components/submit";
+// import { addProduct } from "@/prisma-db";
+// import { redirect } from "next/navigation";
+import { useActionState } from "react";
+import { FormState, createProduct } from "@/actions/products";
+
+// type Errors = {
+//   title?: string;
+//   price?: string;
+//   description?: string;
+// };
+
+// type FormState = {
+//   errors: Errors;
+// };
+
+export default function AddProductPage() {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const [state, formAction, isPending] = useActionState(
+    //when we use useActionState and pass in createProduct, function automatically receives
+    //previous state as the first argument
+    createProduct,
+    initialState
+  );
+
+  //   async function createProduct(formData: FormData) {
+  //     "use server";
+  //     const title = formData.get("title") as string;
+  //     const price = formData.get("price") as string;
+  //     const description = formData.get("description") as string;
+
+  //     const errors: Errors = {};
+
+  //     if (!title) {
+  //       errors.title = "Title is required";
+  //     }
+
+  //     if (!price) {
+  //       errors.price = "Price is required";
+  //     }
+
+  //     if (!description) {
+  //       errors.description = "Description is required";
+  //     }
+
+  //     if (Object.keys(errors).length > 0) {
+  //       //   return { errors: errors };
+  //       return { errors };
+  //     }
+
+  //     await addProduct(title, parseInt(price), description);
+  //     redirect("/products-db");
+  //   }
 
   return (
-    <form action={createProduct} className="p-4 space-y-4 max-w-96">
-      <label className="text-black">
-        Title
-        <input
-          type="text"
-          className="block w-full p-2 text-black border rounded"
-          name="title"
-        />
-      </label>
-      <label className="text-black">
-        Price
-        <input
-          type="number"
-          className="block w-full p-2 text-black border rounded"
-          name="price"
-        />
-      </label>
-      <label className="text-black">
-        Description
-        <textarea
-          className="block w-full p-2 text-black border rounded"
-          name="description"
-        />
-      </label>
-      <Submit />
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <div>
+        <label className="text-black">
+          Title
+          <input
+            type="text"
+            className="block w-full p-2 text-black border rounded"
+            name="title"
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-black">
+          Price
+          <input
+            type="number"
+            className="block w-full p-2 text-black border rounded"
+            name="price"
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-black">
+          Description
+          <textarea
+            className="block w-full p-2 text-black border rounded"
+            name="description"
+          />
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="block w-full p-2 text-white bg-blue-500 disabled:bg-gray-500"
+        disabled={isPending}
+      >
+        Submit
+      </button>
+      {/* <Submit /> */}
     </form>
   );
 }
