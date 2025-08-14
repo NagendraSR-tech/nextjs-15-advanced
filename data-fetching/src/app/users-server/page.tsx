9853a38c48e12b7d89dd7f42ab519b4fce81d482
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-14 12:47:14 +0530

    Fetching data - server components

---
+++ data-fetching/src/app/users-server/page.tsx
@@ -0,0 +1,26 @@
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default async function UsersServer() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+  console.log(users);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <strong>{user.name}</strong>
+          <div>{user.username}</div>
+          <div>{user.email}</div>
+          <div>{user.phone}</div>
+        </li>
+      ))}
+    </ul>
+  );
+}
